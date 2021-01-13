d8ed9d2d0f70ef31da635b9ecc2c12cac350cea7
Detlef Dumpelmann <dumpelmann.detlef@gmail.com>
2021-01-13 13:32:59 +0100

    EventHandler/ listener setup

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 import { movies } from "./moviesDatabase.js";
+
+let selectedMovies = [];
+
+function eventHandler(event) {
+  console.log(event.target.value);
+  if (event.target.value == "nieuwste") {
+    console.log("Je wilt de nieuwste films?");
+    selectedMovies = movies.filter((movie) => {
+      if (movie.Year >= 2014) {
+        return movie;
+      }
+    });
+  } else {
+    console.log(event.target.value);
+    selectedMovies = movies.filter((movie) => {
+      if (movie.Title.includes(event.target.value)) {
+        return movie;
+      }
+    });
+  }
+  console.log(selectedMovies);
+}
+
 document.getElementById("nieuwste").addEventListener("click", eventHandler);
 document.getElementById("avengers").addEventListener("click", eventHandler);
 document.getElementById("x-men").addEventListener("click", eventHandler);
 document.getElementById("princess").addEventListener("click", eventHandler);
 document.getElementById("batman").addEventListener("click", eventHandler);
-
-function eventHandler(event) {
-  console.log(event.target.value);
-  console.log(movies[0].Title);
-}
+document.getElementById("textsearch").addEventListener("input", eventHandler);
